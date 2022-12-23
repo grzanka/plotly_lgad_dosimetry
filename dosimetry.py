bf2833649d8bb15bd8b11ea3e24c21c8409198fb
Leszek Grzanka <grzanka@agh.edu.pl>
2022-12-23 22:05:15 +0100

    fixes

--- dosimetry.py
+++ dosimetry.py
@@ -6,7 +6,7 @@ import plotly.express as px
 from io import StringIO
 
 
-DOSIMETRIC_DATA_URL = "https://gist.githubusercontent.com/grzanka/ac78b7aaea89ec94ac8692842778569e/raw/ada3d728cdc884708dba3e0dab4b92ab89b58347/data.csv"
+DOSIMETRIC_DATA_URL = "https://gist.githubusercontent.com/grzanka/ac78b7aaea89ec94ac8692842778569e/raw/23e988abb7d7892cb13b3937b296ff79a0b474ee/gistfile2.txt"
 
 def fetch_csv(data_url: str) -> pd.DataFrame:
     return pd.read_csv(StringIO(requests.get(data_url).text))
@@ -14,16 +14,16 @@ def fetch_csv(data_url: str) -> pd.DataFrame:
 def plot(df: pd.DataFrame, outfile: Path):
     fig = px.scatter(
         df,
-        x="file_creation_timestamp",
+        x="first_timestamp",
         y=["E1"],
         title="Test",
         color='filename_core',
         facet_col="driver",
         render_mode='webgl'
     )
-    fig.update_traces(marker_line=dict(width=1, color='DarkSlateGray'))
-    fig.write_html(str(outfile), include_plotlyjs='cdn')
-    #fig.show()
+#    fig.update_traces(marker_line=dict(width=1, color='DarkSlateGray'))
+#    fig.write_html(str(outfile), include_plotlyjs='cdn')
+    fig.show()
 
 if __name__ == "__main__":
     df = fetch_csv(DOSIMETRIC_DATA_URL)
